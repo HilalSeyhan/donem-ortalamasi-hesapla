b8845cc786ea33ea3b5b76710fc06cf23da6c84b
HilalSeyhan <82835837+HilalSeyhan@users.noreply.github.com>
2021-09-13 12:45:51 +0300

    ortalamaHesapla-app

---
+++ dinamik_ortalama_hesapla/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:dinamik_ortalama_hesapla/widgets/ortalama_app.dart';
+import 'package:flutter/material.dart';
+
+import 'constants/app_constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dönem Ortalamanı Hesapla',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Sabitler.anaRenk,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: OrtalamaHesapla(),
+    );
+  }
+}
